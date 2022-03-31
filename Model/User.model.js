294eb8dc5bf877911d47740e6e11993baf3a520c
ahmetyanik <ahmetyank4242@gmail.com>
2022-03-31 13:03:37 +0200

    User model is created

---
+++ Model/User.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const {model,Schema} = mongoose;
+
+const userSchema = new Schema({
+
+    name:{
+        required:true,
+        type:String
+    },
+    surname:{
+        required:true,
+        type:String
+    },
+    email:{
+        required:true,
+        type:String
+    },
+    password:{
+        required:true,
+        type:String
+    },
+    address:{
+        type:String
+    },
+    phone:{
+        type:String
+    }
+    
+})
+
+const User = model("user",userSchema,"user");
+
+module.exports = User;
