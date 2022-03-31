18c13fde220fd3681e3bb0b9865796320ada78bf
siddik <spolatde@gmail.com>
2022-03-31 21:48:50 +0200

    product model controller and route is created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const productRouter = require ('./routes/product.router')
 
 //cors
 const cors = require ('cors')
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
+app.use('/', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
