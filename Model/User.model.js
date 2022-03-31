08d66d629bbada64b56e1965461ee62d7c9a8416
ahmetyanik <ahmetyank4242@gmail.com>
2022-03-31 14:06:42 +0200

    UserModel is created

--- Model/User.model.js
+++ Model/User.model.js
@@ -1,33 +1,31 @@
 import mongoose from "mongoose";
-const {model,Schema} = mongoose;
+const { model, Schema } = mongoose;
 
 const userSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  surname: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
 
-    name:{
-        required:true,
-        type:String
-    },
-    surname:{
-        required:true,
-        type:String
-    },
-    email:{
-        required:true,
-        type:String
-    },
-    password:{
-        required:true,
-        type:String
-    },
-    address:{
-        type:String
-    },
-    phone:{
-        type:String
-    }
-    
-})
-
-const User = model("user",userSchema,"user");
+const User = model("user", userSchema, "user");
 
 module.exports = User;
