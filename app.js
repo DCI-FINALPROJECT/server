505637eb6102b5783c4e608847a2a29a02358cf6
ahmetyanik <ahmetyank4242@gmail.com>
2022-04-04 22:03:08 +0200

    Passport.js is for google created

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+const passportSetup = require("./helpers/passport");
+const cookieSession = require("cookie-session");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productsRouter = require("./routes/product.router");
+const authRoute = require("./Middleware/auth.middleware");
+
+
 
 //cors
 const cors = require ('cors');
-const { User,userSchema } = require('./Model/User.model');
 
 var app = express();
 
@@ -29,6 +31,8 @@ app.use(cors(corsOptions))
 
 
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -39,9 +43,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use("/", productsRouter);
+
+app.use("/auth", authRoute);
+
 
 
 // catch 404 and forward to error handler
---
+++ helpers/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+console.log(GOOGLE_CLIENT_ID);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+
+
