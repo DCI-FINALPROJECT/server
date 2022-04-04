3f6436ccc77212403fe575863139198eb2772504
siddik <spolatde@gmail.com>
2022-04-04 11:51:00 +0200

    product controller repaired

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -55,39 +55,8 @@ const addProduct = async (req, res) => {
     
     }catch(err){
         res.send(err)
-      
-  try {
-    if (
-      !(
-        data.productName &&
-        data.category &&
-        data.brand &&
-        data.price &&
-        data.description &&
-        data.images &&
-        data.quantities
-      )
-    ) {
-      return res.status(401).send("missing information");
-    }
-
-    const product = new Product({
-      productName: data.productName,
-      category: data.category,
-      brand: data.brand,
-      price: data.price,
-      description: data.description,
-      images: data.images,
-      quantities: data.quantities,
-      reviews: data.reviews,
-      stars: data.stars,
-    });
-
-    await product.save();
-    return res.status(200).send(product);
-  } catch (err) {
-    res.send(err);
-  }
+    } 
+  
 };
 
-module.exports = { addProduct, getProductById };
+module.exports = {addProduct, getProductById} 
