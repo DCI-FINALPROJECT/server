7c2f75c5f107169e1c2657b15ace1ca52fecbeb7
ahmetyanik <ahmetyank4242@gmail.com>
2022-04-04 10:50:49 +0200

    Password is hashed with bcrypt

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,5 @@
 const { User } = require("../Model/User.model");
+const bcrypt = require("bcrypt");
 
 
 // UNCOMPLEATED POST REQ FOR USER
@@ -7,7 +8,9 @@ const { User } = require("../Model/User.model");
 const addNewUser = async (req, res) => {
   try {
 
-    const newUser = req.body
+    const {name, surname,email,password,address,phone} = req.body
+    const hashedPassword = await bcrypt.hash(password,10);
+    const newUser = {name, surname,email,password:hashedPassword,address,phone}
     await User.create(newUser);
 
     res.send("New User is created...");
