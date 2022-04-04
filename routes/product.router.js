496a5d53c2fc02ee13e0d48e41eb44ad18762302
siddik <spolatde@gmail.com>
2022-04-04 11:03:28 +0200

    product validator is added

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 const {addProduct} = require("../controller/product.controller")
 
-router.post("/addproduct", addProduct);
+
+
+router.post("/addproduct",
+
+//Validation
+  body("productName").exists().withMessage("productName can not be empty"),
+  body("category").exists().withMessage("category can not be empty"),
+  body("brand").exists().withMessage("brand can not be empty"),
+  body("price").exists().isFloat().withMessage("price can not be empty or price should be a float"),
+  body("description").exists().withMessage("description can not be empty"),
+  body("images").exists().withMessage("images can not be empty"),
+  body("quantities").exists().withMessage("quantities can not be empty"),
+  body("stars").isFloat({ min: 1.0, max: 5.0 }).withMessage("stars should be between 1 und 5"),
+
+
+addProduct);
 
 module.exports= router;
