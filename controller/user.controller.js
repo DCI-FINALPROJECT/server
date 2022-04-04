1d5e8aef419c9dc4c50ea0fd3c2022e38885aec1
ahmetyanik <ahmetyank4242@gmail.com>
2022-04-04 11:24:32 +0200

    Express-Validator is ready for user/newUser

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,19 +1,38 @@
 const { User } = require("../Model/User.model");
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
-
-// UNCOMPLEATED POST REQ FOR USER
 
 
 const addNewUser = async (req, res) => {
+  const errors = validationResult(req);
+  const isValid = errors.isEmpty();
+
   try {
 
-    const {name, surname,email,password,address,phone} = req.body
-    const hashedPassword = await bcrypt.hash(password,10);
-    const newUser = {name, surname,email,password:hashedPassword,address,phone}
-    await User.create(newUser);
 
-    res.send("New User is created...");
+    if(isValid){
+      const { name, surname, email, password, address, phone } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = {
+        name,
+        surname,
+        email,
+        password: hashedPassword,
+        address,
+        phone,
+      };
+      await User.create(newUser);
+  
+      res.send("New User is created...");
+    }else{
+     return res.status(406).json({
+        message:"Invalid Input",
+        errors:errors.array()
+      })
+    }
+
+
   } catch (err) {
     res.json({
       status: "Error",
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,34 @@
-var express = require('express');
-const { addNewUser } = require('../controller/user.controller');
+var express = require("express");
+const { addNewUser } = require("../controller/user.controller");
 var router = express.Router();
+const { body } = require("express-validator");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 // POST Create a new User
 
-router.post("/newUser", addNewUser);
+router.post(
+  "/newUser",
+
+  body("name").exists().withMessage("Person can not be empty!"),
+  body("surname").exists().withMessage("Person can not be empty!"),
+  body("email")
+    .exists()
+    .isEmail()
+    .withMessage(
+      "Email can not be empty and It must be written in the correct format!"
+    ),
+  body("password")
+    .exists()
+    .isLength({ min: 6 })
+    .withMessage(
+      "Password can not be empty and should be minimum 6 characters!"
+    ),
+
+  addNewUser
+);
 
 module.exports = router;
