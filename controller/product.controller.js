19bd2eac7ea50c2a68876edbfb78b234b256022f
siddik <spolatde@gmail.com>
2022-04-19 13:47:56 +0200

    product edit function is working

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -2,23 +2,18 @@ const Product = require("../Model/Product.model.js");
 const { validationResult } = require("express-validator");
 const filterProductsWithProductName = require("../helpers/filterProductsWithProductName.js");
 
-
-const getAllProducts = async(req,res)=>{
-
-  try{
-
+const getAllProducts = async (req, res) => {
+  try {
     const allProducts = await Product.find();
 
-    res.json(allProducts)
-
-  }catch(err){
+    res.json(allProducts);
+  } catch (err) {
     res.json({
-      status:404,
-      message:err
-    })
+      status: 404,
+      message: err,
+    });
   }
-}
-
+};
 
 const getProductById = async (req, res) => {
   try {
@@ -47,12 +42,46 @@ const getFiveNewestProduct = async (req, res) => {
     });
   }
 };
+
+//UPDATE Product
+const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  try {
+    /* const findedProduct= await Product.findById({_id:id})
+    console.log(findedProduct); */
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      data
+      /* {
+        productName: data.productName ? data.productName : findedProduct.productName,
+        category: data.category ? data.category : findedProduct.category,
+        capacity: data.capacity ? data.capacity : findedProduct.capacity,
+        brand: data.brand,
+        description: data.description ? data.description : findedProduct.description,
+        images: data.images ? data.images : findedProduct.images,
+        price: data.price,
+      } */
+    );
+    res
+      .status(200)
+      .json({ message: "product is updated", product: updatedProduct });
+  } catch (err) {
+    res.status(404).json({
+      status: "404",
+      message: err,
+    });
+  }
+};
+
+//DELETE Product
 const deleteProduct = async (req, res) => {
   const id = req.params.id;
   console.log(id);
 
   try {
-    const deletedProduct =await Product.findByIdAndDelete({ _id: id });
+    const deletedProduct = await Product.findByIdAndDelete({ _id: id });
     res
       .status(200)
       .json({ message: "product is deleted", product: deletedProduct });
@@ -103,7 +132,6 @@ const addProduct = async (req, res) => {
         capacity: data.capacity,
         stock: data.stock,
         productNameWithCapacity: data.productName + "-" + data.capacity,
-
       });
 
       await product.save();
@@ -125,9 +153,7 @@ const getLatestProducts = async (req, res) => {
   try {
     const foundedProducts = await Product.find().limit(20).sort({ _id: -1 });
 
-
     const result = await filterProductsWithProductName(foundedProducts); // HELPER USED HERE :) SUPEERRR
-
 
     res.json(result);
   } catch (err) {
@@ -144,7 +170,7 @@ const getBestSellers = async (req, res) => {
   try {
     const foundedProducts = await Product.find().sort({ sales: -1 });
 
-    const result = await filterProductsWithProductName(foundedProducts)
+    const result = await filterProductsWithProductName(foundedProducts);
 
     res.json(result);
   } catch (err) {
@@ -164,15 +190,13 @@ const getSimilarProducts = async (req, res) => {
     const id = params.id;
     const productName = params.productName;
 
-
     const similarProducts = await Product.find({
       category: category,
       _id: { $nin: id },
-      productName:{$nin:productName}
+      productName: { $nin: productName },
     }).limit(6);
 
     const result = await filterProductsWithProductName(similarProducts);
-
 
     res.json(result);
   } catch (err) {
@@ -187,7 +211,6 @@ const getSimilarProducts = async (req, res) => {
 
 const getBrandsFromDataBase = async (req, res) => {
   try {
-
     const result = (
       await Product.find({ category: req.params.category })
     ).reduce((acc, cur) => {
@@ -231,7 +254,7 @@ const getProductByCapacity = async (req, res) => {
       capacity: params.capacity,
     });
 
-    res.status(200).json({id:findingElement._id.toString()});
+    res.status(200).json({ id: findingElement._id.toString() });
   } catch (err) {
     res.status(404).json({
       status: 404,
@@ -250,5 +273,6 @@ module.exports = {
   getBrandsFromDataBase,
   getBestSellers,
   getProductByCapacity,
-  getAllProducts
+  getAllProducts,
+  updateProduct,
 };
