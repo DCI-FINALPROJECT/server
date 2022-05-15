b5f49395356bb51bba1d972013dae124161d0166
siddik <spolatde@gmail.com>
2022-05-15 02:26:42 +0200

    coupon is running but not right

--- controller/coupon.controller.js
+++ controller/coupon.controller.js
@@ -27,18 +27,38 @@ const getCoupon = async (req, res) => {
   }
 };
 
+const getGift = async (req, res) => {
+try{
+
+  const {giftNumber} = req.body
+  console.log(giftNumber);
+  const findingCoupon = await Coupon.findOne({ giftNumber, isUsed:false });
+  console.log("findingCoupon", findingCoupon);
+  res.json({couponNumber:findingCoupon.couponNumber})
+}catch(err){
+  res.status(404).json({
+    success: false,
+    message: err,
+  });
+
+}
+
+}
+
 const postCoupon = async (req, res) => {
   try {
     const { couponAmount, howMuchCoupon } = req.body;
+    console.log(typeof(couponAmount));
 
     for (let i = 0; i < howMuchCoupon; i++) {
       const couponNumber = `SmS${couponAmount}-${Math.floor(
         Math.random() * 99999999
       )}`;
-
+      const giftNumber= couponAmount==="100"?"GC100":couponAmount==="20"?"GC20":couponAmount==="25"?"GC25":"GC50"
       const newCoupon = {
         couponNumber,
         couponAmount,
+        giftNumber,
         isUsed: false,
       };
       await Coupon.create(newCoupon);
@@ -56,4 +76,5 @@ const postCoupon = async (req, res) => {
 module.exports = {
   getCoupon,
   postCoupon,
+  getGift
 };
--- routes/coupon.router.js
+++ routes/coupon.router.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getCoupon, postCoupon } = require("../controller/coupon.controller");
+const { getCoupon, postCoupon, getGift } = require("../controller/coupon.controller");
 const router = express.Router();
 
 
 
 
 router.get("/admin/getCoupon/:couponNumber", getCoupon);
+
+
+router.post("/getGift", getGift);
 
 
 router.post("/admin/postCoupon", postCoupon);
